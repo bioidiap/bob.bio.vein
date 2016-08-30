c2e7505031a9a3eb89881a3e019824713a0cbe57
Olegs NIKISINS <onikisins@italix03.idiap.ch>
2016-08-30 10:55:57 +0200

    Moved the topography_cut_roi preprocessor initialization to different location

---
+++ bob/bio/vein/configurations/preprocessors.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from ..preprocessors import FingerCrop
+from ..preprocessors.TopographyCutRoi import TopographyCutRoi
+none = FingerCrop()
+he = FingerCrop(postprocessing='HE')
+hfe = FingerCrop(postprocessing='HFE')
+circgabor = FingerCrop(postprocessing='CircGabor')
+
+topography_cut_roi = TopographyCutRoi( blob_xywh_offsets = [ 1, 1, 1, 1 ], 
+                                 filter_name = "medianBlur", 
+                                 mask_size = 7, 
+                                 topography_step = 20, 
+                                 erode_mask_flag = False, 
+                                 convexity_flag = True )
