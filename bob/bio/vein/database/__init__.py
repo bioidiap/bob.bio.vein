40663ea802aa932eeb4d1a224f2e56cd88123a2a
Teodors Eglitis <teodors.eglitis@idiap.ch>
2016-09-27 13:38:40 +0200

    High level interface for BIOWAVE_V1

---
+++ bob/bio/vein/database/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from .database import VeinBioFile
+from .biowave_test import BiowaveTestBioDatabase
+from .biowave_v1 import BiowaveV1BioDatabase
+from .verafinger import VerafingerBioDatabase
+from .utfvp import UtfvpBioDatabase
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
