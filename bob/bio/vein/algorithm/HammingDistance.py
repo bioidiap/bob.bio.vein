24bc61829c370832e355884ce67a2898f4b44d76
Vedrana KRIVOKUCA <vkrivokuca@italix32.idiap.ch>
2017-10-18 18:15:51 +0200

    Make HammingDistance class plus add test

--- bob/bio/vein/algorithm/HammingDistance.py
+++ bob/bio/vein/algorithm/HammingDistance.py
@@ -1,8 +1,24 @@
-# Calculates the Hamming distance (proportion of mismatching corresponding bits) between two binary vectors
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
 
-import bob.bio.base
+
+from bob.bio.base.algorithm import Distance
 import scipy.spatial.distance
-algorithm = bob.bio.base.algorithm.Distance(
-    distance_function = scipy.spatial.distance.hamming,
-    is_distance_function = False  # setting this to False ensures that Hamming distances are returned as positive values rather than negative
-)
+
+
+class HammingDistance (Distance):
+  """Finger vein matching: Hamming Distance between binary fingervein feature vectors
+  """
+
+  def __init__(
+      self,
+      distance_function = scipy.spatial.distance.hamming,
+      is_distance_function = False  # setting this to False ensures that Hamming distances are returned as positive values rather than negative
+  ):
+
+    # Call base class constructor
+    Distance.__init__(
+        self,
+        distance_function = distance_function,
+        is_distance_function = is_distance_function
+    )
